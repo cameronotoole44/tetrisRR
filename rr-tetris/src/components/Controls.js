89c759aa671d22912d33d67ada0734c25f92dd35
cameronotoole44 <cameronotoole404@gmail.com>
2024-06-25 00:06:29 -0400

    controls w buttons

---
+++ rr-tetris/src/components/Controls.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { moveDown, moveLeft, moveRight, rotate } from '../actions';
+
+export default function Controls(props) {
+    const dispatch = useDispatch();
+    const isRunning = useSelector((state) => state.game.isRunning);
+    const gameOver = useSelector((state) => state.game.gameOver);
+
+    return (
+        <div className={`controls`}>
+
+            <button
+                disabled={!isRunning || gameOver}
+                className="control-button"
+                onClick={(e) => {
+                    if (!isRunning || gameOver) { return }
+                    dispatch(moveLeft())
+                }}>Left</button>
+
+
+            <button
+                disabled={!isRunning || gameOver}
+                className="control-button"
+                onClick={(e) => {
+                    if (!isRunning || gameOver) { return }
+                    dispatch(moveRight())
+                }}>Right</button>
+
+
+            <button
+                disabled={!isRunning || gameOver}
+                className="control-button"
+                onClick={(e) => {
+                    if (!isRunning || gameOver) { return }
+                    dispatch(rotate())
+                }}>Rotate</button>
+
+
+            <button
+                disabled={!isRunning || gameOver}
+                className="control-button"
+                onClick={(e) => {
+                    if (!isRunning || gameOver) { return }
+                    dispatch(moveDown())
+                }}>Down</button>
+
+        </div>
+
+    )
+};
