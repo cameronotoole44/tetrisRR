1adcd665e5aaba02ab081cdeec49a810a38fc35e
cameronotoole44 <cameronotoole404@gmail.com>
2024-06-25 00:08:22 -0400

    +

---
+++ rr-tetris/src/components/NextBlock.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import GridSquare from './GridSquare';
+import { useSelector } from 'react-redux';
+import { shapes } from '../utils';
+
+// SHOWS WHICH BLOCK IS NEXT // 
+export default function NextBlock(props) {
+    const nextShape = useSelector((state) => state.game.nextShape)
+    const box = shapes[nextShape][0]
+
+    const grid = box.map((rowArray, row) => {
+        return rowArray.map((square, col) => {
+            return <GridSquare key={`${row}${col}`} color={square} />
+        })
+    })
+
+    return (
+        <div className="next-block">
+            {grid}
+        </div>
+    )
+};
