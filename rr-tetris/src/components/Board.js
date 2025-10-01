dc708f1156318b2258d7eb1b8f02d2c2f640e6a7
wintercoder <winter_coder404@proton.me>
2025-10-01 11:40:21 -0400

    import useCallback

--- rr-tetris/src/components/Board.js
+++ rr-tetris/src/components/Board.js
@@ -1,65 +1,68 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useCallback } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { moveDown } from '../actions';
 import GridSquare from './GridSquare';
 import { shapes } from '../utils';
 
+export default function Board() {
+  const requestRef = useRef(null);
+  const lastUpdateTimeRef = useRef(0);
+  const progressTimeRef = useRef(0);
 
-export default function Board(props) {
-    const requestRef = useRef();
-    const lastUpdateTimeRef = useRef(0);
-    const progressTimeRef = useRef(0);
-    const dispatch = useDispatch();
-    const game = useSelector((state) => state.game);
-    const { grid, shape, rotation, x, y, isRunning, speed } = game;
+  const dispatch = useDispatch();
+  const game = useSelector((state) => state.game);
+  const { grid, shape, rotation, x, y, isRunning, speed } = game;
 
-    const update = (time) => {
-        requestRef.current = requestAnimationFrame(update)
-        if (!isRunning) {
-            return
-        }
-        if (!lastUpdateTimeRef.current) {
-            lastUpdateTimeRef.current = time
-        }
-        const deltaTime = time - lastUpdateTimeRef.current
-        progressTimeRef.current += deltaTime
-        if (progressTimeRef.current > speed) {
-            dispatch(moveDown())
-            progressTimeRef.current = 0
-        }
-        lastUpdateTimeRef.current = time
+  const update = useCallback((time) => {
+    requestRef.current = requestAnimationFrame(update);
+
+    if (!isRunning) return;
+
+    if (!lastUpdateTimeRef.current) {
+      lastUpdateTimeRef.current = time;
     }
 
-    useEffect(() => {
-        requestRef.current = requestAnimationFrame(update)
-        return () => cancelAnimationFrame(requestRef.current)
-    }, [isRunning])
+    const deltaTime = time - lastUpdateTimeRef.current;
+    progressTimeRef.current += deltaTime;
 
-    const block = shapes[shape][rotation];
-    const blockColor = shape;
+    if (progressTimeRef.current > speed) {
+      dispatch(moveDown());
+      progressTimeRef.current = 0;
+    }
 
-    const gridSquares = grid.map((rowArray, row) => {
+    lastUpdateTimeRef.current = time;
+  }, [isRunning, speed, dispatch]);
 
-        return rowArray.map((square, col) => {
-            const blockX = col - x
-            const blockY = row - y
-            let color = square
+  useEffect(() => {
+    lastUpdateTimeRef.current = 0;
+    progressTimeRef.current = 0;
 
-            if (blockX >= 0 && blockX < block.length && blockY >= 0 && blockY < block.length) {
-                color = block[blockY][blockX] === 0 ? color : blockColor
-            }
+    requestRef.current = requestAnimationFrame(update);
+    return () => {
+      if (requestRef.current) cancelAnimationFrame(requestRef.current);
+    };
+  }, [update, isRunning]);
 
-            const k = row * grid[0].length + col;
+  const block = shapes[shape][rotation];
+  const blockColor = shape;
 
-            return <GridSquare
-                key={k}
-                color={color} />
-        })
+  const gridSquares = grid.map((rowArray, row) =>
+    rowArray.map((square, col) => {
+      const blockX = col - x;
+      const blockY = row - y;
+      let color = square;
+
+      if (
+        blockX >= 0 && blockX < block.length &&
+        blockY >= 0 && blockY < block.length
+      ) {
+        color = block[blockY][blockX] === 0 ? color : blockColor;
+      }
+
+      const k = row * grid[0].length + col;
+      return <GridSquare key={k} color={color} />;
     })
+  );
 
-    return (
-        <div className='board'>
-            {gridSquares}
-        </div>
-    )
-};
+  return <div className="board">{gridSquares}</div>;
+}
